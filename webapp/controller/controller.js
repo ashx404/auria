8ddb0df1ace0215d729deae071f42568ab12b03e
AgamSingh <agam.singh1999@gmail.com>
2019-12-03 20:50:29 +0530

    api updated

--- webapp/controller/controller.js
+++ webapp/controller/controller.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
 const axios = require("axios");
+
+let lat = 28.7;
+let lon = 77.1;
+let temp, water, humidity, elevation;
+
+let tempVal;
+let precipVal;
+let seaVal;
+let heightVal;
+
 module.exports.sendapival = (req, res) => {
   let lat = req.body.lat;
   let lon = req.body.lon;
-  console.log(lat);
-  console.log(lon);
   async function findWeather() {
-    // *****COUNTRY**********
+    // **COUNTRY***
     let countryData;
     try {
-      let countryUrl = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=`;
+      let countryUrl = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=e46770635aa44ffbbdf6807b600b344d`;
       countryData = await axios.get(countryUrl);
       countryData =
         countryData["data"]["results"]["0"]["components"]["country"];
@@ -19,128 +27,79 @@ module.exports.sendapival = (req, res) => {
 
     console.log("Name  " + countryData);
 
-    // * DIST FROM SEA *********
-    let distFromSea;
+    // * DIST FROM SEA ***
     try {
       let seaData = await axios.get(
-        `https://api.kbgeo.com/coastal-distance/v2/coord?lat=${lat}&lng=${lon}`,
-        {
-          params: {},
-          headers: { "kb-auth-token": "" }
-        }
+        "https://api.onwater.io/api/v1/results/${lat},${lon}"
       );
-      distFromSea = seaData["data"]["distanceInMiles"] * 1.6; //in km
-      console.log("seaDist  " + distFromSea + "km");
+      water = seaData["water"];
     } catch (err) {
-      distFromSea = 0;
+      water = false;
     }
 
-    console.log(distFromSea + "  SEA");
+    console.log(water);
 
-    //******POPULATION DENSITY******
-
-    let popData;
-    try {
-      let popUrl = `http://www.foodsecurityportal.org/api/countries/population-density/${countryData}.csv`;
-      popData = await axios.get(popUrl);
-      popData = popData["data"];
-      popData = popData.split(",");
-      popData = popData.slice(-1)[0];
-      popData = Number(popData.split('"')[1]);
-    } catch (error) {
-      if (distFromSea == 0) {
-        popData = 0;
-      } else {
-        popData = 250;
-      }
-    }
-    if (popData == 2016 && distFromSea == 0) {
-      popData = 0;
-    }
-    if (popData == 2016) {
-      popData = 250;
-    }
-    console.log("POP  " + popData);
-
-    //*****
-    // *******TEMP AND RAINFALL*********
-    let rainData = await axios.get(
-      `https://api.darksky.net/forecast/{apikey}/${lat},${lon}`
+    let TEMP = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=e22c5633dc0e79e6fe9dc29dd57e51e4`
     );
-    let currTemp = rainData["data"]["currently"]["temperature"];
-    let precipIntensity = rainData["data"]["currently"]["precipIntensity"];
-    currTemp = [5 * (currTemp - 32)] / 9;
+    temp = TEMP["data"]["main"]["temp"];
+    console.log(temp);
+    humidity = TEMP["data"]["main"]["humidity"];
 
-    console.log("precipIntensity  " + precipIntensity);
-    console.log("currTemp  " + currTemp);
-   
+    console.log(humidity);
+    let eleUrl = await axios.get(
+      `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lon}&units=metric&key=AIzaSyDcnHYgcC-bKhg4sIumw-FooUjSHMBFods`
+    );
+    elevation = eleUrl["data"]["results"][0]["elevation"];
+    console.log(elevation);
 
-    //z3r0
+    if (temp > 35) {
+      tempVal = 4;
+    } else if (temp > 20) {
+      tempVal = 3;
+    } else if (temp > 5) {
+      tempVal = 2;
+    } else {
+      tempVal = 1;
+    }
+
+    if (water == false) {
+      seaVal = 0;
+    } else {
+      seaVal = 1;
+    }
+    if (humidity > 80) {
+      precipVal = 4;
+    } else if (humidity > 55) {
+      precipVal = 3;
+    } else if (humidity > 30) {
+      precipVal = 2;
+    } else {
+      precipVal = 1;
+    }
+
+    if (elevation > 1000) {
+      heightVal = 4;
+    } else if (elevation > 500) {
+      heightVal = 3;
+    } else if (elevation > 200) {
+      heightVal = 2;
+    } else {
+      heightVal = 1;
+    }
+
+    console.log(tempVal + "." + precipVal + "." + seaVal + "." + heightVal);
+
+    res.json({
+      tempVal: data,
+      seaVal: seaVal,
+      // precipVal: precipVal,
+      popVal: popVal
+    });
   }
   findWeather();
-
-  let tempVal;
-  let precipVal;
-  let seaVal;
-  let popVal;
-
-  //TEMP
- 
-  if (currTemp > 35) {
-    tempVal = 4;
-  } else if (currTemp > 20) {
-    tempVal = 3;
-  } else if (currTemp > 5) {
-    tempVal = 2;
-  } else {
-    tempVal = 1;
-  }
-
-  //DIST FROM SEA
-  if (this.distFromSea > 2000) {
-    seaVal = 4;
-  } else if (this.distFromSea > 1000) {
-    seaVal = 3;
-  } else if (this.distFromSea > 500) {
-    seaVal = 2;
-  } else {
-    seaVal = 1;
-  }
-
-  //POPULATION
-  if (this.popData > 500) {
-    popVal = 4;
-  } else if (this.popData > 250) {
-    popVal = 3;
-  } else if (this.popData > 100) {
-    popVal = 2;
-  } else {
-    popVal = 1;
-  }
-  let arr = [1, 2, 3, 4];
-  let data = arr[Math.floor(Math.random() * arr.length)];
-  let data2 = arr[Math.floor(Math.random() * arr.length)];
-
-  // if (this.currTemp > 35) {
-  //     tempVal = 4;
-  // }
-  // else if (currTemp > 20) {
-  //     tempVal = 3;
-  // }
-  // else if (currTemp > 5) {
-  //     tempVal = 2;
-  // }
-  // else {
-  //     tempVal = 1;
-  // }
-
-  res.json({
-    tempVal: data,
-    seaVal: seaVal,
-    // precipVal: precipVal,
-    popVal: popVal
-  });
 };
+
 module.exports.gethome = (req, res) => {
   let home = fs.readFileSync("./public/home.html");
   let home1 = home + " ";
